942aab281353f898f9f83bd23025cdba21589b75
erplt <34658085+erplt@users.noreply.github.com>
2021-12-20 16:13:27 +0100

    update

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace BlazorApp
             builder.RootComponents.Add<App>("#app");
 
             //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://sphere-api.azurewebsites.net/api/") });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://sphere-relay.azurewebsites.net/api/") });
             // Create singleton for Application State
             builder.Services.AddSingleton<Services.AppState>();
             await builder.Build().RunAsync();
